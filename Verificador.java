7b94f6741a3b2d4e646250a8d137a10596e645dd
Nino-FR-04 <164616658+Nino-FR-04@users.noreply.github.com>
2025-03-21 23:35:06 -0500

    Comentarios en Verificador.java

--- Verificador.java
+++ Verificador.java
@@ -12,7 +12,8 @@ public class Verificador {
         double x2 = Math.max(r1.getEsquina1().getX(),r1.getEsquina2().getX());
         double x3 = Math.min(r2.getEsquina1().getX(),r2.getEsquina2().getX());
         double x4 = Math.max(r2.getEsquina1().getX(),r2.getEsquina2().getX());
-    
+
+        //Verifican si hay algun tipo de separacion en cualquiera de los ejes
         boolean sepY = y2 <= y3 || y4 <= y1;
         boolean sepX = x2 <= x3 || x4 <= x1;
 
@@ -32,7 +33,10 @@ public class Verificador {
         double x2 = Math.max(r1.getEsquina1().getX(),r1.getEsquina2().getX());
         double x3 = Math.min(r2.getEsquina1().getX(),r2.getEsquina2().getX());
         double x4 = Math.max(r2.getEsquina1().getX(),r2.getEsquina2().getX());
-        
+
+        /*Validan si hay algun tipo de separacion en cualquiera de los ejes
+        ademas de validar si los ejes X o Y de los rectangulos estan juntos
+        */
         boolean juntX = (x2 == x3 || x4 == x1) && !(y2 < y3 || y4 < y1);
         boolean juntY = (y2 == y3 || y4 == y1) && !(x2 < x3 || x4 < x1); 
         
@@ -41,6 +45,7 @@ public class Verificador {
     }
 
     public static boolean esDisjunto(Rectangulo r1,Rectangulo r2){   
+        //Verifica que no ocurra ninguno de los dos casos
         return !Verificador.esSobrePos(r1, r2) && !Verificador.esJunto(r1, r2);
     }
 }
